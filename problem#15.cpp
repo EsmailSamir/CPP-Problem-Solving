6ab12fa6f04d636b084939b5ec7efc50f374f3e4
Esmail Samir <ismaelsamer11@gmail.com>
2026-06-13 00:16:05 +0300

    Solve Problem 15 with strict input validation and clean architecture

---
+++ problem#15.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+void readlength(float &height, float &width)
+{
+    cout << "\nEnter Height: ";
+    cin >> height;
+    cout << "Enter Width : ";
+    cin >> width;
+    while (cin.fail() || height <= 0 || width <= 0)
+    {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cout << "Enter A Positev Numeric Value\n"
+             << "Enter Height: ";
+        cin >> height;
+        cout << "Enter Width: ";
+        cin >> width;
+    }
+}
+float calculaterectangle(const float &height, const float &width)
+{
+    float rectangle = height * width;
+    return rectangle;
+}
+void printrectangle(float rectangle)
+{
+    cout << "=======================\n"
+         << "Rectangle = " << rectangle << '\n';
+}
+int main()
+{
+    float height, width;
+    readlength(height, width);
+    printrectangle(calculaterectangle(height, width));
+    return 0;
+}
